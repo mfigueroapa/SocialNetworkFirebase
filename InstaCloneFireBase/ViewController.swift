bb2f8b9a7c8c76c841a688e5a3a0b770d26246c2
Mauricio Figueroa <mfigueroapa@hotmail.com>
2020-02-27 17:56:07 -0600

    first commit

---
+++ InstaCloneFireBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaCloneFireBase
+//
+//  Created by Mauricio Figueroa on 27/02/20.
+//  Copyright © 2020 Mauricio Figueroa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

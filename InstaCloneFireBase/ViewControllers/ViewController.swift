7c9bace5daa27359806c71afed47762d282bee27
Mauricio Figueroa <mfigueroapa@hotmail.com>
2020-02-27 21:25:05 -0600

    authentication done

--- InstaCloneFireBase/ViewControllers/ViewController.swift
+++ InstaCloneFireBase/ViewControllers/ViewController.swift
@@ -19,6 +19,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+      
     }
 
     @IBAction func signInClicked(_ sender: Any) {

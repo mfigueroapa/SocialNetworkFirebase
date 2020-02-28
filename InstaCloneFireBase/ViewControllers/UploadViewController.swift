92405bf827c6b4711aed60ea1d09298727ff918e
Mauricio Figueroa <mfigueroapa@hotmail.com>
2020-02-27 21:51:01 -0600

    imagePickerController Done

--- InstaCloneFireBase/ViewControllers/UploadViewController.swift
+++ InstaCloneFireBase/ViewControllers/UploadViewController.swift
@@ -8,12 +8,32 @@
 
 import UIKit
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentText: UITextField!
+    @IBOutlet weak var uploadButton: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
 
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func chooseImage() {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
     
 
@@ -27,4 +47,6 @@ class UploadViewController: UIViewController {
     }
     */
 
+    @IBAction func actionButtonClicked(_ sender: Any) {
+    }
 }

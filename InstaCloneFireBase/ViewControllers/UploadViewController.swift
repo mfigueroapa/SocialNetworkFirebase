f4c075144201bab76b11a94a0667114ffefa0998
Mauricio Figueroa <mfigueroapa@hotmail.com>
2020-02-28 20:37:24 -0600

    serverTimeStamp added to date

--- InstaCloneFireBase/ViewControllers/UploadViewController.swift
+++ InstaCloneFireBase/ViewControllers/UploadViewController.swift
@@ -83,11 +83,17 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
                             
                             var firestoreReference : DocumentReference? = nil
                             
-                            let firestorePost = ["imageUrl" : imageUrl!, "postedBy" : Auth.auth().currentUser!.email, "postComment" : self.commentText.text!, "date": "date", "likes" : 0] as [String : Any]
+                            let firestorePost = ["imageUrl" : imageUrl!, "postedBy" : Auth.auth().currentUser!.email, "postComment" : self.commentText.text!, "date": FieldValue.serverTimestamp(), "likes" : 0] as [String : Any]
                             
                             firestoreReference = firestoreDatabase.collection("Posts").addDocument(data: firestorePost, completion: { (error) in
                                 if error != nil {
                                     self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                                    
+                                } else {
+                                    
+                                    self.imageView.image = UIImage()
+                                    self.commentText.text = ""
+                                    self.tabBarController?.selectedIndex = 0 //index of tabBarController
                                 }
                             })
                             

d057e047fff3883b324c488e1f6cf7b4997ecd77
Mauricio Figueroa <mfigueroapa@hotmail.com>
2020-03-01 11:58:59 -0600

    downloaded data from FireStore

--- InstaCloneFireBase/ViewControllers/FeedViewController.swift
+++ InstaCloneFireBase/ViewControllers/FeedViewController.swift
@@ -7,31 +7,69 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
 
     @IBOutlet weak var tableView: UITableView!
     
+    var userEmailArray = [String]()
+    var userCommentArray = [String]()
+    var likeArray = [Int]()
+    var userImageArray = [String]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        getFireStoreData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return userEmailArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "feedCell", for: indexPath) as! FeedTableViewCell
-        cell.userEmailLabel.text = "userEmail"
-        cell.likeLabel.text = " 000 "
-        cell.commentLabel.text = "texttt"
+        cell.userEmailLabel.text = userEmailArray[indexPath.row]
+        cell.likeLabel.text = String(likeArray[indexPath.row])
+        cell.commentLabel.text = userCommentArray[indexPath.row]
         cell.userImageView.image = UIImage()
         return cell//
         
+    }
+    
+    func getFireStoreData() {
+        let fireStoreDatabase = Firestore.firestore()
+        fireStoreDatabase.collection("Posts").addSnapshotListener { (snapshot, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                if snapshot?.isEmpty != true && snapshot != nil {
+                    for document in snapshot!.documents { //contains all of the documents of the posts collection
+                        let documentID = document.documentID
+              
+                        if let postedBy = document.get("postedBy") as? String { //casting
+                            self.userEmailArray.append(postedBy)
+                        }
+                        if let postComment = document.get("postComment") as? String {
+                            self.userCommentArray.append(postComment)
+                        }
+                        if let likes = document.get("likes") as? Int {
+                            self.likeArray.append(likes)
+                        }
+                        if let imageUrl = document.get("imageUrl") as? String {
+                            self.userImageArray.append(imageUrl)
+                        }
+                    }
+                    
+                    self.tableView.reloadData()
+                }
+               
+            }
+        }
     }
     
     

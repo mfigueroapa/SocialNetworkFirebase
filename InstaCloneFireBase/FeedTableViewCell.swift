361f15f8fc7b119132769effef245e2a91ead1ba
Mauricio Figueroa <mfigueroapa@hotmail.com>
2020-03-01 11:33:19 -0600

    FeedTableViewCell added

---
+++ InstaCloneFireBase/FeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedTableViewCell.swift
+//  InstaCloneFireBase
+//
+//  Created by Mauricio Figueroa on 01/03/20.
+//  Copyright © 2020 Mauricio Figueroa. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
